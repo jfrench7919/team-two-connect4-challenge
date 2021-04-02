18d876e1f86964ac6c77574cf9dc8cc69210d9f6
Aaron Kunzer <aaron.kunzer.ctr@navy.mil>
2021-04-02 14:10:30 -0400

    added more logic

--- angular-app/src/app/components/connect-four/connect-four.component.ts
+++ angular-app/src/app/components/connect-four/connect-four.component.ts
@@ -12,18 +12,22 @@ export class ConnectFourComponent implements OnInit {
   board: BoardColumn[] = [];
   winner: boolean = false;
   draw: boolean = false;
+  playCount = 0;
 
+  readonly boardColumns = 7;
+  readonly boardRows = 6;
   readonly playerOneColor = 'red';
   readonly playerTwoColor = 'yellow';
+  readonly neutralColor = 'white';
 
   constructor() { }
 
   ngOnInit(): void {
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < this.boardColumns; i++) {
       this.board.push(new BoardColumn());
-      for (var j = 0; j < 6; j++) {
+      for (var j = 0; j < this.boardRows; j++) {
         const chip = new PlayerChip()
-        chip.colorOfSpace = 'white';
+        chip.colorOfSpace = this.neutralColor;
         this.board[i].playerChips[j] = chip;
       }
     }
@@ -32,26 +36,63 @@ export class ConnectFourComponent implements OnInit {
   startGame() {
     this.board.forEach(col => {
       col.playerChips.forEach((row) => {
-        row.colorOfSpace = 'white';
+        row.colorOfSpace = this.neutralColor;
       });
     });
+    this.isPlayerOne = true;
+    this.winner = false;
+    this.draw = false;
+    this.playCount = 0;
   }
 
   addChip(column: number) {
-    
+
+    this.playCount++;
+    this.endTurn();
   }
 
   endTurn() {
-    this.checkBoardForWin();
+    if (this.checkBoardForWin()) {
+      this.winner = true;
+      return;
+    }
+
+    if (this.playCount === this.boardColumns * this.boardRows) {
+      this.draw = true;
+      return;
+    }
+
     this.isPlayerOne = !this.isPlayerOne;
   }
 
-  checkBoardForWin() {
-    // Check verticals
+  checkBoardForWin(): boolean {
+    const checkColor = this.isPlayerOne ? this.playerOneColor : this.playerTwoColor;
 
-    // Check diagonals
+    this.board.forEach((col, i) => {
+      col.playerChips.forEach((row, j) => {
+        if (row.colorOfSpace !== checkColor) {
+          return;
+        }
 
-    // Check Horizonals
+        // Right Horizonal
+        if (i < this.boardColumns - 3) {
+
+        }
+
+        // Left Horizontal
+        // Upper Vertical
+        // Lower Vertical
+        // Diagonal Down Right
+        // Diagonal Down Left
+        // Diagonal Up Right
+        // Diagonal Up Left
+
+        
+      })
+
+    });
+
+    return false;
   }
 
 
